4e8fe46f986f8bedc5d7b1db2a63b0fe88da4f67
tejas <tejasshah@gmail.com>
2022-08-06 16:15:41 +0530

    init

---
+++ src/main/java/com/bean/RoleBean.java
@@ -0,0 +1,26 @@
+package com.bean;
+
+//ORM 
+//class object == relation  == map 
+
+public class RoleBean {
+	private int roleId;
+	private String roleName;
+
+	public int getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getRoleName() {
+		return roleName;
+	}
+
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
+
+}
