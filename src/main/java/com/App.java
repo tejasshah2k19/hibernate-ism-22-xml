91869544763c19be2cb13ba3415d02d5bebac692
tejas <tejasshah@gmail.com>
2022-08-06 17:06:58 +0530

    fix

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,13 +1,25 @@
 package com;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		// use to create session factory from give configuration
+		Configuration cfg = new Configuration().configure("hibernate-config.xml");
+
+		// use to create session
+		SessionFactory factory = cfg.buildSessionFactory();
+
+		// use to execute query
+		Session session = factory.openSession();//every time open 
+
+	}
 }
